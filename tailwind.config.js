eb64548ebb74bca62cdf9902550054d538bdefb6
Joseb Beridze <beridzeioseb05@gmail.com>
2024-05-30 17:17:53 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors:{
+        "dark-bg": "#141c2e",
+        "dark-gray": "#1e2b48",
+        "dark-blue": "#007afd"
+      }
+    },
+  },
+  plugins: [],
+}
+
